131312505417554b8e9d692bf87ef412a5b9c672
Derek Wade <derek.wade@responsiveX.com>
2021-01-14 12:33:11 -0600

    fix broken unit test

--- src/CiCdLearning/Controllers/WeatherForecastController.cs
+++ src/CiCdLearning/Controllers/WeatherForecastController.cs
@@ -26,15 +26,14 @@ namespace CiCdLearning.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            return new List<WeatherForecast>();
-            // var rng = new Random();
-            // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            // {
-            //     Date = DateTime.Now.AddDays(index),
-            //     TemperatureC = rng.Next(-20, 55),
-            //     Summary = Summaries[rng.Next(Summaries.Length)]
-            // })
-            // .ToArray();
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
         }
     }
 }
